dd0794e1204db24e10674688793a126a56dbaa4f
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2024-04-14 17:48:10 +0300

    extract  db check to func

--- main.go
+++ main.go
@@ -29,46 +29,16 @@ var embeddedData embed.FS
 var db *sql.DB
 
 func init() {
-
-	tempDir, err := os.MkdirTemp("", "data")
+	homeDir, err := getDBPath()
+	dbPath := filepath.Join(homeDir, "data.db")
 	if err != nil {
-		log.Fatal("Error creating temporary directory:", err)
+		log.Println("error loading data.db")
 	}
 
-	// Extract the embedded data directory to the temporary directory
-	if err := extractEmbeddedData(tempDir); err != nil {
-		log.Fatal("Error extracting embedded data:", err)
-	}
-
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal("Error getting user's home directory:", err)
-	}
-
-	// Define the directory path
-	dirPath := filepath.Join(homeDir, ".n2media")
-
-	// Check if the directory exists
-	_, err = os.Stat(dirPath)
-	if os.IsNotExist(err) {
-		// Directory does not exist, create it
-		err := os.MkdirAll(dirPath, 0777)
-		if err != nil {
-			log.Fatal("Error creating directory:", err)
-		}
-		log.Println("Directory created:", dirPath)
-	} else if err != nil {
-		// Error occurred while checking directory existence
-		log.Fatal("Error checking directory:", err)
-	} else {
-		// Directory already exists
-		log.Println("Directory already exists:", dirPath)
-	}
-
-	dbPath := filepath.Join(homeDir, ".n2media", "data.db")
-	db, err = sql.Open("sqlite3", dbPath)
-	if err != nil {
-		log.Fatal("Error opening SQLite database:", err)
+	var erro error
+	db, erro = sql.Open("sqlite3", dbPath)
+	if erro != nil {
+		log.Fatal("Error opening SQLite database:", erro)
 	}
 
 	// Initialize tables if they don't exist
@@ -118,7 +88,6 @@ func main() {
 
 // createTables initializes the necessary tables in the SQLite database
 func createTables() error {
-	log.Print("@createTables")
 	_, err := db.Exec(`
 		CREATE TABLE IF NOT EXISTS libraries (
 			name TEXT PRIMARY KEY,
@@ -143,7 +112,6 @@ func extractEmbeddedData(targetDir string) error {
 	}
 
 	for _, file := range files {
-		log.Println("reading file", file.Name())
 		data, err := embeddedData.ReadFile(fmt.Sprintf("data/%s", file.Name()))
 		if err != nil {
 			return err
@@ -153,4 +121,47 @@ func extractEmbeddedData(targetDir string) error {
 		}
 	}
 	return nil
+}
+
+func getDBPath() (string, error) {
+
+	tempDir, err := os.MkdirTemp("", "data")
+	if err != nil {
+		log.Fatal("Error creating temporary directory:", err)
+	}
+
+	// Extract the embedded data directory to the temporary directory
+	if err := extractEmbeddedData(tempDir); err != nil {
+		log.Fatal("Error extracting embedded data:", err)
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Error getting user's home directory:", err)
+	}
+
+	// Define the directory path
+	dirPath := filepath.Join(homeDir, ".n2media")
+
+	// Check if the directory exists
+	_, err = os.Stat(dirPath)
+	if os.IsNotExist(err) {
+		// Directory does not exist, create it
+		err := os.MkdirAll(dirPath, 0777)
+		if err != nil {
+			log.Fatal("Error creating directory:", err)
+			return "", err
+		}
+		log.Println("Directory created:", dirPath)
+		return dirPath, nil
+	} else if err != nil {
+		// Error occurred while checking directory existence
+		log.Fatal("Error checking directory:", err)
+		return "", err
+	} else {
+		// Directory already exists
+		log.Println("Directory already exists:", dirPath)
+		return dirPath, nil
+	}
+
 }
